fb7b1ef990f001153e9420123cada100e6a63e00
Rafael Silva Aleixo <rafael.aleixo@vericode.com.br>
2022-06-13 13:53:33 -0300

    Implementacao inicial

---
+++ DoubleCheck/DoubleCheck/Classes/Linha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleCheck.Classes
+{
+    public class Linha
+    {
+        public List<string[]> texto { get; set; } = new List<string[]>();
+        public List<Efeitos> ListaEfeitos = new List<Efeitos>();
+    }
+}
---
+++ DoubleCheck/DoubleCheck/Form1.cs
@@ -0,0 +1,100 @@
+﻿using DoubleCheck.Classes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DoubleCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            string[] efeitos = new string[999];
+            string[] linhas = new string[9999];
+            char delimiterChar = '\n';
+            List<Linha> listaLinhasArq1 = new List<Linha>();
+            List<Linha> listaLinhasArq2 = new List<Linha>();
+
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string fileName;
+                fileName = dlg.FileName;
+                string dados = File.ReadAllText(fileName);
+                configuraLinhaEfeitos(ref efeitos, delimiterChar, listaLinhasArq1, dados);
+            }
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string fileName;
+                fileName = dlg.FileName;
+                string dados = File.ReadAllText(fileName);
+                configuraLinhaEfeitos(ref efeitos, delimiterChar, listaLinhasArq2, dados);
+            }
+
+            //TODO: Comparar listaLinhasArq1 e listaLinhasArq2
+        }
+
+        private static void configuraLinhaEfeitos(ref string[] efeitos, char delimiterChar, List<Linha> listaLinhasArq1, string dados)
+        {
+            string[] linhas = dados.Split(delimiterChar);
+            string infoLinha = string.Empty;
+            foreach (string linha in linhas)
+            {
+                if (!string.IsNullOrEmpty(linha))
+                {
+                    Linha linhaAtual = new Linha();
+                    string texto = PegarConteudoForaAspas(linha, 0);
+                    string[] configTexto = texto.Split(';');
+                    linhaAtual.texto.Add(configTexto);
+
+                    Efeitos efeitoLinha = new Efeitos();
+                    string efeitoStr = PegarConteudoEntreAspas(linha, 0);
+                    efeitos = efeitoStr.Split('|');
+                    foreach (string efeito in efeitos)
+                    {
+                        string[] efeitoConfigurado = efeito.Split(';');
+                        efeitoLinha.efeitos.Add(efeitoConfigurado);
+                    }
+                    linhaAtual.ListaEfeitos.Add(efeitoLinha);
+                    listaLinhasArq1.Add(linhaAtual);
+                }
+            }
+        }
+
+        static string PegarConteudoEntreAspas(string texto, int linha)
+        {
+            int i = 0;
+            for (int x = 0; x < linha; x++) i = texto.IndexOf("\n", i) + 1; // Encontra a linha...
+            int j = texto.IndexOf("\n", i); // Encontra o limite da linha
+            if (j < 0) j = texto.Length - 1;
+
+            string strLinha = texto.Substring(i, j - i); // A linha selecionada
+            i = strLinha.IndexOf("\"") + 1;
+            j = strLinha.IndexOf("\"", i);
+            string teste = strLinha.Remove(i, j - i);
+
+            return strLinha.Substring(i, j - i);
+        }
+        static string PegarConteudoForaAspas(string texto, int linha)
+        {
+            int i = 0;
+            for (int x = 0; x < linha; x++) i = texto.IndexOf("\n", i) + 1; // Encontra a linha...
+            int j = texto.IndexOf("\n", i); // Encontra o limite da linha
+            if (j < 0) j = texto.Length - 1;
+
+            string strLinha = texto.Substring(i, j - i); // A linha selecionada
+            i = strLinha.IndexOf("\"") + 1;
+            j = strLinha.IndexOf("\"", i);
+
+            return strLinha.Remove(i, j - i);
+        }
+    }
+}
